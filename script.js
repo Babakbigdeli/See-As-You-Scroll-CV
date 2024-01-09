3b33275cc76f0ad64d3e894640731ec02dba847d
Babak <babakbigdeli@gmail.com>
2024-01-09 17:14:06 +0000

    minor content update

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const captions = [
     attrToInsert: 
     `<p>My name is pronounced bɑːˈb&aelig;k, as if you merge the words bob&back on those adjacent Bs.</p>
     <p>I reside in Manchester and by submitting this CV, I'm seeking positions in Web Development. </p>
-    <p>It's only a five minutes read. I've tried to keep it short and concise. You're not going to sit there scrolling forever.</p>`,
+    <p>It's only a four minutes read. I've tried to keep it concise. You're not going to sit there scrolling forever.</p>`,
     },
     {
     target: 'languages',
@@ -58,7 +58,7 @@ const captions = [
     attrToInsert:
     `<p>It happened during the lockdown. Over a span of 24 weeks, I engaged in an intensive bootcamp program that emphasized 
     industry-leading practices and pragmatic software development standards.<br>Throughout the course, 
-    I actively&nbsp;learned how to craft and launch full-stack web applications. 
+    I actively&nbsp;learned how to make and launch full-stack web applications. 
     My learning involved exploring new languages and frameworks through collaborative efforts with peers, 
     resulting in the development of nearly 20 small applications.</p>`,
     },
@@ -72,21 +72,18 @@ const captions = [
     target: 'Intellithing',
     insertionDiv: 'backgroundRight',
     attrToInsert: 
-    `<p>Collaborating with a team of front-end developers on the construction of a web portal for an AI and LLM project. 
-    The project is being developed using React, Typescript, and Tailwind. We are adhering to an AGILE approach with a SCRUM team collaboration framework 
-    and a GitHub Project task-board system. During intervals between sprints, I engage with the Digital Marketing team, contributing
+    `<p>Collaborated with a team of front-end developers on the construction of a web portal for an AI and LLM project. 
+    The project is developed using React, Typescript, and Tailwind. We adhered to an AGILE approach with a SCRUM team collaboration framework 
+    and a GitHub Project task-board system. During intervals between sprints, I engaged with the Digital Marketing team, contributing
     to marketing campaigns utilizing tools like Linkedin Sales Navigator and Pipedrive.</p>`,
     },
     {
     target: 'opensupplies',
-    insertionDiv: 'foregroundRight',
+    insertionDiv: 'backgroundRight',
     attrToInsert:
     `<p>
-    My initial exposure to Shopify marked my entrance into the world of eCommerce. Overseeing the management of two Shopify stores, I delved into 
-    the intricacies of the Liquid templating language, crafting themes, and tailoring the storefronts' visual identity. Our extensive online 
-    presence spanned multiple platforms, including Amazon, eBay, EKM, Kaufland, Mirakle ecosystem, Cdiscount, and others.</p>
-    <p>My responsibilities were diverse and encompassed catalog maintenance, market research, sourcing new products for inclusion, price optimization 
-    to ensure competitiveness, comprehensive product analysis and sales assessment. This experience was an invaluable journey that provided 
+    My initial exposure to Shopify marked my entrance into the world of eCommerce. Overseeing the management of two Shopify stores, I explored
+    the Liquid templating language, crafting themes, and tailoring the storefronts' visual identity. This experience was an invaluable journey that provided 
     me with an in-depth understanding of the eCommerce landscape, one that I thoroughly enjoyed.</p>`,
     },
     {
@@ -95,8 +92,8 @@ const captions = [
     attrToInsert:
     `I bring forth over 20 years of extensive experience in administrative and logistics roles, spanning from clerical positions to managerial
     responsibilities across various companies. This diverse journey has not only equipped me with a wide array of skills but has also honed my 
-    adaptability. From high-pressure scenarios in remote islands to corporate environments within bustling metropolises, 
-    I've successfully navigated both extremes of work settings. Check my Linkedin profile (link follows) for details.`,
+    adaptability. From high-pressure heavy industrial site works on remote islands to corporate environments within bustling metropolises, 
+    I've successfully experienced both extremes of work settings. Check my Linkedin profile (link follows) for details.`,
     },
     {
     target: 'personalProfile',
@@ -109,10 +106,10 @@ const captions = [
     <p>Fact: In the majority of companies I've been part of, I've received a promotion within my first year of employment.</p>`,
     },
     {target: 'summary',
-    insertionDiv: 'foregroundRight',
+    insertionDiv: 'backgroundRight',
     attrToInsert:
     `<p>I consider myself an experienced professional with a wealth of skills, versatility, and flexibility. Applying for this position reflects my 
-    confidence in meeting the job requirements and performing up to the expected standards. I refrain from mass applications for various reasons. 
+    confidence in meeting the job requirements and performing up to the expected standards. 
     I possess the attitude and approach needed for success. Skills, if I don't have them, I will learn them in no time.
     I am convinced I would make a valuable addition to your team.</p>`,
     },
@@ -122,7 +119,8 @@ const captions = [
     attrToInsert:
     `<p>Phone: 07456184955</p>
     <p>Email: <a href="mailto:babakbigdeli@gmail.com">babakbigdeli@gmail.com</a></p>
-    <p>LinkedIn: <a style="text-align: left; font-size: 36px;" href="https://www.linkedin.com/in/babak-bigdeli-3b999055"; target="_blank">https://www.linkedin.com/in/babak-bigdeli-3b999055</a></p>
+    <p>LinkedIn: <a style="text-align: left; font-size: 36px;" href="https://www.linkedin.com/in/babak-bigdeli-3b999055"; target="_blank">Click Here</a></p>
+    <p>Github:&nbsp;<a style="text-align: left; font-size: 36px;" href="https://github.com/Babakbigdeli"; target="_blank">Click Here</a></p>
     <p>My Printable CV: <a style="text-align: left; font-size: 36px;" href="https://drive.google.com/file/d/1R6taMOMiAz7SLNdGrI4IHz0Xtnj5d7LQ/view?usp=sharing"; target="_blank">Click Here</a></p>
     `,
     },
